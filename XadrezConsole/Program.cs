5665c2dd0df05dcbf55b362ed4dbaa3d9db297a8
Felipe Duarte <reis.felipeduarte@gmail.com>
2021-06-14 13:19:00 -0300

    Posições no tabuleiro

---
+++ XadrezConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using XadrezConsole.tabuleiro;
+
+namespace XadrezConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+        }
+    }
+}
