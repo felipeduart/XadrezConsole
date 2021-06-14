5944e1f791333f59e6340e895bf58a82ecdef0f1
Felipe Duarte <reis.felipeduarte@gmail.com>
2021-06-14 15:41:30 -0300

    Colocando a logica de posicao do xadrez

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -9,22 +9,11 @@ namespace XadrezConsole
     {
         static void Main(string[] args)
         {
-            try
-            {
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            Console.WriteLine(pos);
 
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            Console.WriteLine(pos.toPosicao());
 
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-
-                Tela.ImprimirTabuleiro(tab);
-            }
-            catch (TabuleiroExcepition e)
-            {
-                Console.WriteLine(e.Message);
-            }
             Console.WriteLine();
         }
     }
